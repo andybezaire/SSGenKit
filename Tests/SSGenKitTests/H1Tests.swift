d775e46ffbd8a97f1ea077196f0465fb0e81324f
andy <andy.bezaire@nordea.com>
2024-02-06 15:38:16 +0200

    Add tag functionality

--- Tests/SSGenKitTests/H1Tests.swift
+++ Tests/SSGenKitTests/H1Tests.swift
@@ -1,8 +1,44 @@
-//
-//  File.swift
-//  
-//
-//  Created by andy on 6.2.2024.
-//
+import XCTest
+import SSGenKit
 
-import Foundation
+final class H1Tests: XCTestCase {
+    func test_printing_containsTag() throws {
+        let sut = makeSUT()
+
+        let html = "\(sut)"
+
+        let openIndex = try XCTUnwrap(html.index(of: "<h1>"))
+        let closeIndex = try XCTUnwrap(html.index(of: "</h1>"))
+
+        XCTAssertTrue(openIndex < closeIndex, "tag open should be before close")
+    }
+
+    // MARK: - helpers
+    private func makeSUT() -> H1 {
+        let element = H1()
+        return element
+    }
+}
+
+// MARK: - move to production
+struct H1 { }
+
+extension H1: CustomStringConvertible {
+    var description: String {
+        """
+<h1>
+</h1>
+"""
+    }
+}
+
+//let k = H1 {
+//    "Thgis is the H1 contents"
+//}
+// print(k)
+//
+//"""
+//"<h1>
+//  Thgis is the H1 contents
+//</h1>
+//"""
