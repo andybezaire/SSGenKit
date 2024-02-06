aac84eaaf98d9596e7cb0571cf23a0ab3e27305e
andy <andy.bezaire@nordea.com>
2024-02-06 16:00:54 +0200

    Add p element

--- Tests/SSGenKitTests/PTests.swift
+++ Tests/SSGenKitTests/PTests.swift
@@ -2,29 +2,48 @@ import XCTest
 import SSGenKit
 
 final class PTests: XCTestCase {
-//    func test_noContent_printing_containsTag() throws {
-//        let sut = makeSUT()
-//
-//        let html = "\(sut)"
-//
-//        let openIndex = try XCTUnwrap(html.index(of: "<h1>"))
-//        let closeIndex = try XCTUnwrap(html.index(of: "</h1>"))
-//
-//        XCTAssertTrue(openIndex < closeIndex, "tag open should be before close")
-//    }
+    func test_noContent_printing_containsTag() throws {
+        let sut = makeSUT()
 
-//    func test_printing_containsContent() throws {
-//        let content = uniqueString()
-//        let sut = makeSUT(content: content)
-//
-//        let html = "\(sut)"
-//
-//        XCTAssertTrue(html.contains(content), "html: \(html) should contain \(content)")
-//    }
+        let html = "\(sut)"
+
+        let openIndex = try XCTUnwrap(html.index(of: "<p>"))
+        let closeIndex = try XCTUnwrap(html.index(of: "</p>"))
+
+        XCTAssertTrue(openIndex < closeIndex, "tag open should be before close")
+    }
+
+    func test_printing_containsContent() throws {
+        let content = uniqueString()
+        let sut = makeSUT(content: content)
+
+        let html = "\(sut)"
+
+        XCTAssertTrue(html.contains(content), "html: \(html) should contain \(content)")
+    }
 
     // MARK: - helpers
-//    private func makeSUT(content: String = "") -> P {
-//        let element = H1 { content }
-//        return element
-//    }
+    private func makeSUT(content: String = "") -> P {
+        let element = P { content }
+        return element
+    }
+}
+
+// MARK: - move to production
+public struct P {
+    private let content: () -> String
+
+    public init(content: @escaping () -> String) {
+        self.content = content
+    }
+}
+
+extension P: CustomStringConvertible {
+    public var description: String {
+        """
+<p>
+\(content())
+</p>
+"""
+    }
 }
