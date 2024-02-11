e714c03c5ea4633d5cab20703c6b5c9cea8b910b
andy <andy.bezaire@nordea.com>
2024-02-11 09:56:59 +0200

    Refactor for clarity

--- Tests/SSGenKitTests/H1Tests.swift
+++ Tests/SSGenKitTests/H1Tests.swift
@@ -1,5 +1,6 @@
 import XCTest
 import SSGenKit
+import InlineSnapshotTesting
 
 final class H1Tests: XCTestCase {
     func test_noContent_printing_containsTag() throws {
@@ -15,16 +16,34 @@ final class H1Tests: XCTestCase {
 
     func test_printing_containsContent() throws {
         let content = uniqueString()
-        let sut = makeSUT(content: content)
+        let sut = makeSUT(content: { content })
 
         let html = "\(sut)"
 
         XCTAssertTrue(html.contains(content), "html: \(html) should contain \(content)")
     }
 
+    func test_init_doesNotExecuteContent() throws {
+        _ = makeSUT(content: { XCTFail() ; return "" })
+    }
+
+//    func test_matchesSnapshot() {
+//        let sut = makeSUT(content: { "This is an h1 tag." })
+//
+//        let html = "\(sut)"
+//
+//        assertInlineSnapshot(of: html, as: .lines) {
+//            """
+//            <h1>
+//              This is an h1 tag.
+//            </h1>
+//            """
+//        }
+//    }
+
     // MARK: - helpers
-    private func makeSUT(content: String = "") -> H1 {
-        let element = H1 { content }
+    private func makeSUT(content: @escaping () -> String = { "" }) -> H1 {
+        let element = H1(content: content)
         return element
     }
 }
