6ac7abf10045f5b175b9269f2becf5aafe3f5589
andy <andy.bezaire@nordea.com>
2024-02-13 20:09:16 +0200

    Move to own file

--- Sources/SSGenKit/Environment/FontModifier.swift
+++ Sources/SSGenKit/Environment/FontModifier.swift
@@ -1,20 +1,3 @@
-public enum HTMLBodyFont: CaseIterable {
-    /// Equivalent to H1
-    case mainHeading
-    /// Equivalent to H2
-    case heading
-    /// Equivalent to H3
-    case subheading
-    /// Equivalent to H4
-    case tertiaryHeading
-    /// Equivalent to H5
-    case quaternaryHeading
-    /// Equivalent to H6
-    case quinaryHeading
-    /// Equivalent to p
-    case body
-}
-
 extension HTMLBodyElement {
     public func font(_ font: HTMLBodyFont) -> HTMLBodyElement {
         return self
