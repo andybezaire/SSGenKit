e64ae1202797ef99922bd30d7645af0e7f264702
andy <andy.bezaire@nordea.com>
2024-02-14 08:17:54 +0200

    Move for clarity

--- Sources/SSGenKit/Elements/HTMLElement.swift
+++ Sources/SSGenKit/Elements/HTMLElement.swift
@@ -1,5 +1,9 @@
 public protocol HTMLElement: CustomStringConvertible { }
 
+// MARK: - head
 public protocol HTMLHeadElement: HTMLElement { }
-
 extension String: HTMLHeadElement { }
+
+// MARK: - body
+public protocol HTMLBodyElement: HTMLElement { }
+extension String: HTMLBodyElement { }
