ce3af175141b33ace6aea88da8767eff0adb0002
andy <andy.bezaire@gmail.com>
2024-02-17 21:12:09 +0200

    Add target api in comments

--- Tests/SSGenKitTests/ExampleUseTests.swift
+++ Tests/SSGenKitTests/ExampleUseTests.swift
@@ -4,49 +4,93 @@ import InlineSnapshotTesting
 
 final class ExampleUseTests: XCTestCase {
 //    func test_() {
-//        WebPage(title: "Homepage") {
-//            VStack {
-//                H1("Jere Teittinen")
-//                    .font(.largeTitle)
-//                P("""
+//        let sut = Website {
+//            Navigation(.top) {
+//                HStack(alignment: .trailing) {
+//                    Link("Home", destination: .home)
+//                    Link("About", destination: .about)
+//                    Link("Blog", destination: .blog)
+//                }
+//            }
+//            .destination(for: .home) {
+//                VStack {
+//                    Text("Jere Teittinen")
+//                        .font(.mainHeading)
+//                    Text(
+//                    """
 //                    Hello. I am an experienced developer who strives to find
 //                    simple solutions to complicated problems
-//""")
+//                    """
+//                    )
+//                }
 //            }
+//            .destination(for: .about) { Text("About") }
+//            .destination(for: .blog) { Text("Blog") }
+//            .title("Homepage")
+//            .foregroundColor(.white)
+//            .background(Color.black)
+//        }
+//
+//        let html = "\(sut)"
+//
+//        assertInlineSnapshot(of: html, as: .lines) {
+//            """
+//            <!DOCTYPE html>
+//            <html>
+//              <head>
+//                <title>
+//                  Homepage
+//                </title>
+//              </head>
+//              <body>
+//                <section style="display:flex;flex-direction:column;align-items:center;">
+//                  <h1>
+//                    Jere Teittinen
+//                  </h1>
+//                  <p>
+//                    Hello. I am an experienced developer who strives to find
+//                    simple solutions to complicated problems
+//                  </p>
+//                </section>
+//              </body>
+//            </html>
+//            """
 //        }
 //    }
 
-    func test_helloWorld_matchesSnapshot() {
-        let sut = WebPage(title: "Welcome, World!") {
-            VStack {
-                Text("Hello, World!")
-                Text("Welcome to the great new World!")
-            }
-        }
-
-        let html = "\(sut)"
-
-        assertInlineSnapshot(of: html, as: .lines) {
-            """
-            <!DOCTYPE html>
-            <html>
-              <head>
-                <title>
-                  Welcome, World!
-                </title>
-              </head>
-              <body>
-                <div style="display:flex;flex-direction:column;align-items:center;">
-                  <p>
-                    Hello, World!
-                  </p>
-                  <p>
-                    Welcome to the great new World!
-                  </p>
-                </div>
-              </body>
-            </html>
-            """
-        }
-    }
+//    func test_helloWorld_matchesSnapshot() {
+//        let sut = WebPage(title: "Welcome, World!") {
+//            VStack {
+//                Text("Hello, World!")
+//                Text("Welcome to the great new World!")
+//            }
+//            .foregroundStyle(.white)
+//            .background(Color.black)
+//        }
+//
+//        let html = "\(sut)"
+//
+//        assertInlineSnapshot(of: html, as: .lines) {
+//            """
+//            <!DOCTYPE html>
+//            <html>
+//              <head>
+//                <title>
+//                  Welcome, World!
+//                </title>
+//              </head>
+//              <body>
+//                <div style="display:flex;flex-direction:column;align-items:center;">
+//                  <p>
+//                    Hello, World!
+//                  </p>
+//                  <p>
+//                    Welcome to the great new World!
+//                  </p>
+//                </div>
+//              </body>
+//            </html>
+//            """
+//        }
+//    }
 }
