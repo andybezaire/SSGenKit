ade4edb23a5627ef13b81d5ae68ad550ca524a5f
andy <andy.bezaire@gmail.com>
2024-02-18 11:48:05 +0200

    Add multiple order tests

--- Tests/SSGenKitTests/Environment/CombiningModifiersTests.swift
+++ Tests/SSGenKitTests/Environment/CombiningModifiersTests.swift
@@ -3,7 +3,7 @@ import SSGenKit
 import InlineSnapshotTesting
 
 final class CombiningModifiersTests: XCTestCase {
-    func test_text_html_matchesSnapshot() {
+    func test_order1Text_html_matchesSnapshot() {
         let sut = Text("Hello, World!")
             .font(.mainHeading)
             .foregroundStyle(.white)
@@ -20,10 +20,44 @@ final class CombiningModifiersTests: XCTestCase {
         }
     }
 
-    func test_order2_text_html_matchesSnapshot() {
+    func test_order2Text_html_matchesSnapshot() {
         let sut = Text("Hello, World!")
             .font(.mainHeading)
             .background(CSSColor.black)
+            .foregroundStyle(.white)
+
+        let html = "\(sut)"
+
+        assertInlineSnapshot(of: html, as: .lines) {
+            """
+            <h1 style="background-color:black;color:white;">
+              Hello, World!
+            </h1>
+            """
+        }
+    }
+
+    func test_order3Text_html_matchesSnapshot() {
+        let sut = Text("Hello, World!")
+            .background(CSSColor.black)
+            .foregroundStyle(.white)
+            .font(.mainHeading)
+
+        let html = "\(sut)"
+
+        assertInlineSnapshot(of: html, as: .lines) {
+            """
+            <h1 style="background-color:black;color:white;">
+              Hello, World!
+            </h1>
+            """
+        }
+    }
+
+    func test_order4Text_html_matchesSnapshot() {
+        let sut = Text("Hello, World!")
+            .background(CSSColor.black)
+            .font(.mainHeading)
             .foregroundStyle(.white)
 
         let html = "\(sut)"
